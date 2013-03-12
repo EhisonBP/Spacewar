64a006d204cfddf05773fa8732db21c51343b529
Ehison  de Jesus Perez Benedetti <ehisonbp@eperez.cnti.gob.ve>
2013-03-12 12:03:06 -0430

    Creando vista home de la APP

--- gen/ve/gob/epb/asteroides/R.java
+++ gen/ve/gob/epb/asteroides/R.java
@@ -24,7 +24,11 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int Boton01=0x7f080000;
+        public static final int Boton02=0x7f080001;
+        public static final int Boton03=0x7f080002;
+        public static final int Boton04=0x7f080003;
+        public static final int action_settings=0x7f080004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -33,9 +37,14 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
+        public static final int AcercaDe=0x7f050005;
+        public static final int Arrancar=0x7f050003;
+        public static final int Configurar=0x7f050004;
+        public static final int Salir=0x7f050006;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int tituloAplicacion=0x7f050007;
     }
     public static final class style {
         /** 
